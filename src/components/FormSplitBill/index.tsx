0c8f2eaa8a90e39e6320da50056891614c138270
Osama Sayed <osamasayedmuhammad@gmail.com>
2023-08-10 19:17:20 +0300

    💁 Add controlled elements‍

--- src/components/FormSplitBill/index.tsx
+++ src/components/FormSplitBill/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Person } from "../../models/person";
 
 import Button from "../shared/Button";
@@ -7,23 +8,59 @@ type FormSplitBillProps = {
 };
 
 const FormSplitBill = ({ selectedFriend }: FormSplitBillProps) => {
+  const [bill, setBill] = useState<string | number>("");
+  const [paidByUser, setPaidByUser] = useState<string | number>("");
+  const [whoIsPaying, setWhoIsPaying] = useState("user");
+
+  const paidByFriend =
+    typeof bill === "number" && typeof paidByUser === "number"
+      ? bill - paidByUser
+      : "";
+
   return (
     <form className="form-split-bill">
       <h2>Split a bill with {selectedFriend.name}</h2>
 
       <label htmlFor="bill-value">💰 Bill value</label>
-      <input id="bill-value" type="text" />
+      <input
+        id="bill-value"
+        type="text"
+        value={bill}
+        onChange={(e) =>
+          setBill(
+            isNaN(+e.target.value) || +e.target.value < +paidByUser
+              ? bill
+              : +e.target.value
+          )
+        }
+      />
 
       <label htmlFor="your-expense">🧍 Your expense</label>
-      <input id="your-expense" type="text" />
+      <input
+        id="your-expense"
+        type="text"
+        value={paidByUser}
+        onChange={(e) =>
+          setPaidByUser(
+            isNaN(+e.target.value) || +e.target.value > +bill
+              ? paidByUser
+              : +e.target.value
+          )
+        }
+      />
 
       <label htmlFor="friend-expense">👬 {selectedFriend.name}'s expense</label>
-      <input id="friend-expense" type="text" disabled />
+      <input id="friend-expense" type="text" disabled value={paidByFriend} />
 
       <label htmlFor="person-bill">🤑 Who is paying the bill?</label>
-      <select name="bill" id="person-bill">
+      <select
+        name="bill"
+        id="person-bill"
+        value={whoIsPaying}
+        onChange={(e) => setWhoIsPaying(e.target.value)}
+      >
         <option value="user">You</option>
-        <option value="X">{selectedFriend.name}</option>
+        <option value="friend">{selectedFriend.name}</option>
       </select>
 
       <Button>Split bill</Button>
